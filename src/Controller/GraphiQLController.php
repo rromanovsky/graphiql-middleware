4539dfb0c7b5e46ab869a55662aec46a58537805
Roman Romanovsky <rromanovsky@klika-tech.com>
2017-11-02 09:17:16 +0300

    Added router

---
+++ src/Controller/GraphiQLController.php
@@ -0,0 +1,12 @@
+<?php
+namespace GraphiQLMiddleware\Controller;
+
+class GraphiQLController {
+    public function __construct()
+    {
+    }
+
+    public function show() {
+        echo 'GraphiQL frontend will be here';
+    }
+}
--- src/GraphiQLMiddleware.php
+++ src/GraphiQLMiddleware.php
@@ -1,9 +1,19 @@
 <?php
 namespace GraphiQLMiddleware;
 
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 class GraphiQLMiddleware {
     public function __construct()
     {
-        echo 'hello world';exit;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null) {
+        if (!$next) {
+            return $response;
+        }
+
+        return $next($request, $response);
     }
 }
