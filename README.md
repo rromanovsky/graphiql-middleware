fe29e839a3b452c8f671d0fb41305d904e205c2a
Roman Romanovsky <rromanovsky@klika-tech.com>
2017-11-04 10:09:18 +0300

    Working version

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # GraphiQL PSR-7 Middleware
 
+You can add a [GraphiQL](https://github.com/graphql/graphiql) interface to your project with this PSR-7 Middleware
+
+[![](src/graphiql/interface.png)](http://graphql.org/swapi-graphql)
+
 ## Install
+```
+composer require rromanovsky/graphiql-middleware
+```
 
 ## Usage
 - Slim
     ```php
     use GraphiQLMiddleware\GraphiQLMiddleware;
-    $app->add(new GraphiQLMiddleware());
+    // 1) Allow middleware to be executed on '/graphiql' route
+    $app->add(new GraphiQLMiddleware(['route' => '/graphiql']));
+    // 2) Allow middleware to be executed on '/graphiql' route
+    $app->get("/graphiql", function($request, $response, $args) {
+        return $response;
+    })->add(new GraphiQLMiddleware(['ingoreRoute' => true]));
+    // 3) Allow middleware to be executed on any route
+    $app->add(new GraphiQLMiddleware(['ingoreRoute' => true]));
     ```
--- src/GraphiQLMiddleware.php
+++ src/GraphiQLMiddleware.php
@@ -4,24 +4,90 @@ namespace GraphiQLMiddleware;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-class GraphiQLMiddleware {
-    private $path = "";
+require_once(__DIR__ . '/../vendor/autoload.php');
 
-    public function __construct($path = "/graphiql")
+class GraphiQLMiddleware
+{
+    /**
+     * @var string
+     */
+    private $route = '';
+
+    /**
+     * @var string
+     */
+    private $graphqlURI = '/graphql';
+
+    /**
+     * @var bool
+     */
+    private $ingoreRoute = false;
+
+    /**
+     * @param array $params
+     *
+     * $params['route']                        string
+     * $params['ingoreRoute']  boolean
+     * $params['graphqlURI']                   string
+     * ]
+     */
+    public function __construct(array $params = [])
     {
-        $this->path = $path;
-    }
-
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null) {
-        if ($request->getUri()->getPath() === $this->path) {
-            echo file_get_contents(__DIR__ . "/graphiql/index.html");
-            exit;
+        if (array_key_exists('route', $params)) {
+            $this->route = $params['route'];
         }
 
-        if (!$next) {
+        if (array_key_exists('ingoreRoute', $params)) {
+            $this->ingoreRoute = $params['ingoreRoute'];
+        }
+
+        if (array_key_exists('graphqlURI', $params)) {
+            $this->graphqlURI = $params['graphqlURI'];
+        }
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param callable|null $next
+     * @return ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+    {
+        if ($request->getMethod() === 'GET' && (
+            $this->ingoreRoute || !empty($this->route) && $this->route === $request->getUri()->getPath()
+        )) {
+            $response->getBody()->write($this->render($request));
+
             return $response;
         }
 
-        return $next($request, $response);
+        return $next ? $next($request, $response) : $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return string
+     */
+    private function render(ServerRequestInterface $request)
+    {
+        $graphqlPath = $this->getGraphqlPath($request);
+        $template = new \Text_Template(__DIR__ . '/graphiql/index.html');
+        $template->setVar(['graphqlPath' => $graphqlPath]);
+
+        return $template->render();
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return mixed
+     */
+    private function getGraphqlPath(ServerRequestInterface $request)
+    {
+        $requestURI = $request->getServerParams()['REQUEST_URI'];
+        $requestPath = $request->getUri()->getPath();
+        $routePosition = strrpos($requestURI, $requestPath);
+
+        return substr_replace($requestURI, $this->graphqlURI, $routePosition, strlen($requestPath));
     }
 }
